a82256804e6ddf0455bdeb0113c08e7ed7b79d01
cytopyge <cytopyge@protonmail.com>
2019-01-05 10:49:23 +0100

    20190105_104923 (autopush)

--- abis2.sh
+++ abis2.sh
@@ -51,7 +51,7 @@ echo '127.0.1.1     "$hostname".localdomain     "$hostname"' >> /etc/hosts
 passwd
 
 ## update repositories and install core applications
-pacman -Sy openssh linux-headers linux-lts linux-lts-headers wpa_supplicant wireless_tools xclip git
+pacman -Sy openssh linux-headers linux-lts linux-lts-headers wpa_supplicant wireless_tools xclip
 
 # installing the EFI boot manager
 ## install boot files
@@ -77,13 +77,13 @@ boot_part=$(findmnt | grep /boot | awk '{print $2}')
 echo 'title Arch Linux BLE' > /boot/loader/entries/arch.conf
 echo 'linux /vmlinuz-linux' >> /boot/loader/entries/arch.conf
 echo 'initrd /initramfs-linux.img' >> /boot/loader/entries/arch.conf
-echo 'options cryptdevice=[UUID=:lvm]/["$boot_part":lvm] crypto=sha512:aes-xts-plain64:512:0: root=/dev/mapper/volgroup0-lv_root resume=/dev/mapper/volgroup0-lv_swap' >> /boot/loader/entries/arch.conf
+echo 'options cryptdevice="$boot_part":lvm crypto=sha512:aes-xts-plain64:512:0: root=/dev/mapper/volgroup0-lv_root resume=/dev/mapper/volgroup0-lv_swap' >> /boot/loader/entries/arch.conf
 
 ## lts kernel
 echo 'title Arch Linux LTS' > /boot/loader/entries/arch-lts.conf
 echo 'linux /vmlinuz-linux-lts' >> /boot/loader/entries/arch-lts.conf
 echo 'initrd /initramfs-linux-lts.img' >> /boot/loader/entries/arch-lts.conf
-echo 'options cryptdevice=[UUID=:lvm]/["$boot_part":lvm] crypto=sha512:aes-xts-plain64:512:0: root=/dev/mapper/volgroup0-lv_root resume=/dev/mapper/volgroup0-lv_swap' >> /boot/loader/entries/arch-lts.conf
+echo 'options cryptdevice="$boot_part":lvm crypto=sha512:aes-xts-plain64:512:0: root=/dev/mapper/volgroup0-lv_root resume=/dev/mapper/volgroup0-lv_swap' >> /boot/loader/entries/arch-lts.conf
 
 # generate initramfs with mkinitcpio
 # for linux preset
