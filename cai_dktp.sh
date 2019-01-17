fefd5dba53cb8d3f7bcee692c12a43646a079ab3
cytopyge <cytopyge@protonmail.com>
2019-01-17 13:22:40 +0100

    20190117_132237 (autopush)

--- cai_dktp.sh
+++ cai_dktp.sh
@@ -3,3 +3,4 @@
 # cytopyge arch linux desktop installation
 
 yay -Sy xorg xorg-xinit rxvt-unicode xterm iw alsa-utils mesa xf86-video-nouveau i3-gaps unclutter
+git clone https://github.com/cytopyge/dotfiles ~/.dot
