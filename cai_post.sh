2048d65451fd272c0aa5e23ef603ac784e5dd652
cytopyge <cytopyge@protonmail.com>
2019-01-17 10:21:54 +0100

    20190117_102152 (autopush)

--- cai_post.sh
+++ cai_post.sh
@@ -8,4 +8,22 @@ pacman -Ql terminus-font
 ## set terminus-font
 setfont ter-v14n
 
+# modify pacman.conf
+## add color
+sed -i 's/#Color/Color/' /etc/pacman.conf
+## add total download counter
+sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
+## add multilib repository
+### [TODO] comment out existing lines
+echo '################################' >> /etc/pacman.conf
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+
+# yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
 
