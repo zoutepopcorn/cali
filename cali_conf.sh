76e023b89f05be52e751174f4ca711933467b43f
cytopyge <cytopyge@protonmail.com>
2019-01-17 17:03:36 +0100

    20190117_170336 (autopush)

---
+++ cali_conf.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+##
+###            _ _                    __ 
+###   ___ __ _| (_)   ___ ___  _ __  / _|
+###  / __/ _` | | |  / __/ _ \| '_ \| |_ 
+### | (_| (_| | | | | (_| (_) | | | |  _|
+###  \___\__,_|_|_|  \___\___/|_| |_|_|  2
+###  _ _|_ _ ._    _  _  
+### (_\/|_(_)|_)\/(_|(/_ 
+###   /      |  /  _|                     
+###
+### cali_conf
+### cytopyge arch linux installation configuration
+### second part of a script series
+### (c) 2019 by cytopyge
+###
+##
+#
+
+# time settings
+## set time zone
+ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+## set hwclock
+hwclock --systohc
+
+# locale settings
+sed -i "/^#en_US.UTF-8 UTF-8/c\en_US.UTF-8 UTF-8" /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+# network configuration
+## create the hostname file
+echo -n 'Enter hostname? '
+read hostname
+echo "$hostname" > /etc/$hostname
+## add matching entries to hosts file
+echo '127.0.0.1    localhost.localdomain    localhost' >> /etc/hosts
+echo '::1    localhost.localdomain    localhost' >> /etc/hosts
+echo '127.0.1.1     "$hostname".localdomain     "$hostname"' >> /etc/hosts
+
+# set root password
+passwd
+
+# update repositories and install core applications
+pacman -Sy openssh linux-headers linux-lts linux-lts-headers wpa_supplicant wireless_tools xclip vim --noconfirm
+
+# installing the EFI boot manager
+## install boot files
+bootctl install
+## boot loader configuration
+echo 'default arch' > /boot/loader/loader.conf
+echo 'timeout 3' >> /boot/loader/loader.conf
+echo 'console-mode max' >> /boot/loader/loader.conf
+
+# configure mkinitcpio
+## create an initial ramdisk environment (initramfs)
+## enable systemd HOOKS
+sed -i "/^HOOKS/c\HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt sd-lvm2 filesystems fsck)" /etc/mkinitcpio.conf
+
+# adding boot loader entries
+## bleeding edge kernel (BLE)
+echo 'title Arch Linux BLE' > /boot/loader/entries/arch.conf
+echo 'linux /vmlinuz-linux' >> /boot/loader/entries/arch.conf
+echo 'initrd /initramfs-linux.img' >> /boot/loader/entries/arch.conf
+echo "options rd.luks.name=`blkid | grep crypto_LUKS | awk '{print $2}' | cut -d '"' -f2`=cryptlvm root=UUID=`blkid | grep lv_root | awk '{print $3}' | cut -d '"' -f2` rw resume=UUID=`blkid | grep lv_swap | awk '{print $3}' | cut -d '"' -f2`" >> /boot/loader/entries/arch.conf
+
+## long term support kernel (LTS)
+echo 'title Arch Linux LTS' > /boot/loader/entries/arch-lts.conf
+echo 'linux /vmlinuz-linux-lts' >> /boot/loader/entries/arch-lts.conf
+echo 'initrd /initramfs-linux-lts.img' >> /boot/loader/entries/arch-lts.conf
+echo "options rd.luks.name=`blkid | grep crypto_LUKS | awk '{print $2}' | cut -d '"' -f2`=cryptlvm root=UUID=`blkid | grep lv_root | awk '{print $3}' | cut -d '"' -f2` rw resume=UUID=`blkid | grep lv_swap | awk '{print $3}' | cut -d '"' -f2`" >> /boot/loader/entries/arch-lts.conf
+
+# default settings for sd-vconsole
+touch /etc/vconsole.conf
+
+# generate initramfs with mkinitcpio
+## for linux preset
+mkinitcpio -p linux
+
+## for linux-lts preset
+mkinitcpio -p linux-lts
+
+# add user
+## username
+echo 'enter username? '
+read username
+useradd -m -g wheel $username
+## password
+passwd $username
+## priviledge escalation for wheel group
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+# exit arch-chroot environment 
+## go back to the archiso environment
+
+echo 'exit'
+echo 'umount -R /mnt'
+echo 'Remove boot medium'
+
+## reboot
+echo 'reboot'
+echo 'run capos if preferred'
---
+++ cali_post.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+##
+###            _ _                   _   
+###   ___ __ _| (_)  _ __   ___  ___| |_ 
+###  / __/ _` | | | | '_ \ / _ \/ __| __|
+### | (_| (_| | | | | |_) | (_) \__ \ |_ 
+###  \___\__,_|_|_| | .__/ \___/|___/\__|3
+###                 |_|                  
+###  _ _|_ _ ._    _  _
+### (_\/|_(_)|_)\/(_|(/_
+###   /      |  /  _|
+###
+### cali_post
+### cytopyge arch linux installation post
+### third part of a script series
+### (c) 2019 by cytopyge
+###
+##
+#
+
+## download terminus-font
+pacman -Sy terminus-font
+## install terminus-font
+pacman -Ql terminus-font
+## set terminus-font
+setfont ter-v14n
+
+# modify pacman.conf
+## add color
+sed -i 's/#Color/Color/' /etc/pacman.conf
+## add total download counter
+sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
+## add multilib repository
+### [TODO] comment out existing lines
+echo '################################' >> /etc/pacman.conf
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+
+# yay
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+sudo mount -o remount,rw /usr
+makepkg -s
+makepkg -i 9.0.1-3-x86_64.pkg.tar.xz
+sudo mount -o remount,ro /usr
+cd
+rm -rf /tmp/yay
